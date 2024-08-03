d89145b78cee52a678efd07dcae70a0c80ebc740
JoeHalasz <jhalasz@yahoo.com>
2024-08-03 13:46:50 -0400

    f

--- Assets/Resources/Scripts/AsteroidStuff/TestMakingMesh.cs
+++ Assets/Resources/Scripts/AsteroidStuff/TestMakingMesh.cs
@@ -7,73 +7,77 @@ public class TestMakingMesh : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // make 8 points for the cube
-        Vector3[] vertices = new Vector3[8];
-        vertices[0] = new Vector3(0, 0, 0);
-        vertices[1] = new Vector3(1, 0, 0);
-        vertices[2] = new Vector3(1, 1, 0);
-        vertices[3] = new Vector3(0, 1, 0);
-        vertices[4] = new Vector3(0, 0, 1);
-        vertices[5] = new Vector3(1, 0, 1);
-        vertices[6] = new Vector3(1, 1, 1);
-        vertices[7] = new Vector3(0, 1, 1);
+        // map called uniqueVertices
+        Dictionary<Vector3, int> uniqueVertices = new Dictionary<Vector3, int>();
+        List<int> indices = new List<int>();
+        int x = 0;
+        int y = 0;
+        int z = 0;
+        int spaceBetween = 1;
 
-        // show them all as small spheres
+        Vector3[] newUniqueVertices = new Vector3[8];
+        newUniqueVertices[0] = new Vector3( x - spaceBetween/3.0f,y + spaceBetween/3.0f,z + spaceBetween/3.0f ); // top face back left
+        newUniqueVertices[1] = new Vector3( x + spaceBetween,y + spaceBetween,z + spaceBetween ); // top face back right
+        newUniqueVertices[2] = new Vector3( x - spaceBetween,y + spaceBetween,z - spaceBetween ); // top face front left    
+        newUniqueVertices[3] = new Vector3( x + spaceBetween,y + spaceBetween,z - spaceBetween ); // top face front right
+        newUniqueVertices[4] = new Vector3( x + spaceBetween,y - spaceBetween,z + spaceBetween ); // bottom face back right
+        newUniqueVertices[5] = new Vector3( x - spaceBetween,y - spaceBetween,z + spaceBetween ); // bottom face back left
+        newUniqueVertices[6] = new Vector3( x + spaceBetween,y - spaceBetween,z - spaceBetween ); // bottom face front right
+        newUniqueVertices[7] = new Vector3( x - spaceBetween,y - spaceBetween,z - spaceBetween ); // bottom face front left
+
         for (int i = 0; i < 8; i++)
         {
             GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            sphere.transform.position = vertices[i];
+            sphere.transform.position = newUniqueVertices[i];
             sphere.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
         }
-        // make 6 face for the cube
-        int[] triangles = new int[36];
-        
-        // left face
-        triangles[12] = 4;
-        triangles[13] = 7;
-        triangles[14] = 3;
-        triangles[15] = 4;
-        triangles[16] = 3;
-        triangles[17] = 0;
-        // right face
-        triangles[18] = 1;
-        triangles[19] = 2;
-        triangles[20] = 6;
-        triangles[21] = 1;
-        triangles[22] = 6;
-        triangles[23] = 5;
-        // top face
-        triangles[24] = 2;
-        triangles[25] = 3;
-        triangles[26] = 7;
-        triangles[27] = 2;
-        triangles[28] = 7;
-        triangles[29] = 6;
-        // bottom face
-        triangles[30] = 0;
-        triangles[31] = 1;
-        triangles[32] = 5;
-        triangles[33] = 0;
-        triangles[34] = 5;
-        triangles[35] = 4;
-        // front face
-        triangles[0] = 0;
-        triangles[1] = 2;
-        triangles[2] = 1;
-        triangles[3] = 0;
-        triangles[4] = 3;
-        triangles[5] = 2;
-        // back face
-        triangles[6] = 4;
-        triangles[7] = 5;
-        triangles[8] = 6;
-        triangles[9] = 4;
-        triangles[10] = 6;
-        triangles[11] = 7;
+
+        int[] poses = new int[8];
+        for (long j = 0; j < 8; j++)
+        {
+            // if newUniqueVertices[j] isnt in uniqueVertices then add it
+            if (!uniqueVertices.ContainsKey(newUniqueVertices[j]))
+            {
+                uniqueVertices[newUniqueVertices[j]] = uniqueVertices.Count;
+            }
+            poses[j] = uniqueVertices[newUniqueVertices[j]];
+        }
+
+        int[] localIndices = new int[36] {
+            0, 1, 2, 2, 1, 3, // top face
+            4, 5, 6, 6, 5, 7, // bottom face
+            0, 2, 5, 5, 2, 7, // left face
+            3, 1, 6, 6, 1, 4, // right face
+            2, 3, 7, 7, 3, 6, // front face
+            1, 0, 4, 4, 0, 5  // back face
+        };
+
+        // if removing 0 - 1 2 5 4 are effected
+        // if removing 1 - 0 2 3 6 4 are effected 
+
+        // 0 - 4
+        // 1 - 5
+        // 2 - 5
+        // 3 - 4
+        // 4 - 4
+        // 5 - 5
+        // 6 - 5
+        // 7 - 4
+
+        for (long j = 0; j < 36; j++)
+        {
+            indices.Add(poses[localIndices[j]]);
+        }
+
+        // add some randomenss to all the verts
+        for (int i = 0; i < newUniqueVertices.Length; i++)
+        {
+            //newUniqueVertices[i] += new Vector3(Random.Range(-spaceBetween/4.0f, spaceBetween/4.0f), Random.Range(-spaceBetween/4.0f, spaceBetween/4.0f), Random.Range(-spaceBetween/4.0f, spaceBetween/4.0f));
+        }
 
         Mesh mesh = new Mesh();
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
+        mesh.vertices = newUniqueVertices;
+        mesh.triangles = indices.ToArray();
         mesh.RecalculateNormals();
         GameObject cube = new GameObject();
         MeshFilter meshFilter = cube.AddComponent<MeshFilter>();
